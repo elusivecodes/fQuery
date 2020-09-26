00aed07c4fa12cbe1eb3eb16bcbebf469ca899f0
elusivecodes <18050480+elusivecodes@users.noreply.github.com>
2020-09-26 16:58:24 +1000

    NPM Fix

--- dist/frost-dom.js
+++ dist/frost-dom.js
@@ -1,5 +1,5 @@
 /**
- * FrostDOM v1.1.5
+ * FrostDOM v1.1.6
  * https://github.com/elusivecodes/FrostDOM
  */
 (function(global, factory) {
--- src/bundle_wrapper.js
+++ src/bundle_wrapper.js
@@ -1,5 +1,5 @@
 /**
- * FrostDOM Bundle v1.1.5
+ * FrostDOM Bundle v1.1.6
  * https://github.com/elusivecodes/FrostCore
  * https://github.com/elusivecodes/FrostDOM
  */
